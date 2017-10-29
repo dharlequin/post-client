9ca2617db0689c4f91f177e7377d6374460cdb2d
dharlequin <dharlequin@gmail.com>
2017-10-29 19:20:35 +0300

    added children navigation; added navigation menu styling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { NavPanelComponent } from './home/nav-panel/nav-panel.component';
-import { MailListComponent } from './home/mail-list/mail-list.component';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {NavPanelComponent} from './home/nav-panel/nav-panel.component';
+import {MailListComponent} from './home/mail-list/mail-list.component';
+import {InboxComponent} from './home/mail-list/inbox/inbox.component';
+import {SentComponent} from './home/mail-list/sent/sent.component';
+import {DraftComponent} from './home/mail-list/draft/draft.component';
+import {SpamComponent} from './home/mail-list/spam/spam.component';
 
 const routes = [
-  { path: '', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  {path: '', component: LoginComponent},
+  {
+    path: 'home', component: HomeComponent,
+    children: [
+      {path: '', component: InboxComponent},
+      {path: 'inbox', component: InboxComponent},
+      {path: 'sent', component: SentComponent},
+      {path: 'draft', component: DraftComponent},
+      {path: 'spam', component: SpamComponent}
+    ]
+  }
 ];
 
 @NgModule({
@@ -20,6 +33,10 @@ const routes = [
     HomeComponent,
     NavPanelComponent,
     MailListComponent,
+    InboxComponent,
+    SentComponent,
+    DraftComponent,
+    SpamComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,4 +45,5 @@ const routes = [
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/home/mail-list/mail-list.component.ts
+++ src/app/home/mail-list/mail-list.component.ts
@@ -7,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MailListComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
 
